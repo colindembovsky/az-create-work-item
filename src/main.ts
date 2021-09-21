4e2ce2ddd556d90e77ffdea474b9924781d9dbdf
Colin Dembovsky <colindembovsky@gmail.com>
2021-09-21 18:04:49 +0000

    Adding semis back for linting

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import * as core from '@actions/core'
-import {createWorkItem} from './work-item-functions'
+import * as core from '@actions/core';
+import {createWorkItem} from './work-item-functions';
 
 async function run(): Promise<void> {
   try {
-    const token: string = core.getInput('token')
-    const orgName: string = core.getInput('orgName')
-    const project: string = core.getInput('project')
-    const type: string = core.getInput('type')
-    const title: string = core.getInput('title')
-    const description: string = core.getInput('description')
-    const areaPath: string = core.getInput('areaPath')
-    const iterationPath: string = core.getInput('iterationPath')
+    const token: string = core.getInput('token');
+    const orgName: string = core.getInput('orgName');
+    const project: string = core.getInput('project');
+    const type: string = core.getInput('type');
+    const title: string = core.getInput('title');
+    const description: string = core.getInput('description');
+    const areaPath: string = core.getInput('areaPath');
+    const iterationPath: string = core.getInput('iterationPath');
 
-    core.debug(`orgName: ${orgName}`)
-    core.debug(`project: ${project}`)
-    core.debug(`type: ${type}`)
-    core.debug(`title: ${title}`)
-    core.debug(`description: ${description}`)
-    core.debug(`areaPath: ${areaPath}`)
-    core.debug(`iterationPath: ${iterationPath}`)
+    core.debug(`orgName: ${orgName}`);
+    core.debug(`project: ${project}`);
+    core.debug(`type: ${type}`);
+    core.debug(`title: ${title}`);
+    core.debug(`description: ${description}`);
+    core.debug(`areaPath: ${areaPath}`);
+    core.debug(`iterationPath: ${iterationPath}`);
 
     const newId = await createWorkItem(token, orgName, project, {
       type,
@@ -26,13 +26,13 @@ async function run(): Promise<void> {
       description,
       areaPath,
       iterationPath
-    })
-    core.info(`Create work item [${title}] with id ${newId}`)
+    });
+    core.info(`Create work item [${title}] with id ${newId}`);
 
-    core.setOutput('workItemId', newId)
+    core.setOutput('workItemId', newId);
   } catch (error) {
-    core.setFailed((error as any)?.message)
+    core.setFailed((error as any)?.message);
   }
 }
 
-run()
+run();
--- src/work-item-functions.ts
+++ src/work-item-functions.ts
@@ -1,13 +1,14 @@
-import * as azdev from 'azure-devops-node-api'
-import {IWorkItemTrackingApi} from 'azure-devops-node-api/WorkItemTrackingApi'
-import {JsonPatchDocument} from 'azure-devops-node-api/interfaces/common/VSSInterfaces'
+import * as azdev from 'azure-devops-node-api';
+import * as core from '@actions/core';
+import {IWorkItemTrackingApi} from 'azure-devops-node-api/WorkItemTrackingApi';
+import {JsonPatchDocument} from 'azure-devops-node-api/interfaces/common/VSSInterfaces';
 
 export interface IWorkItemInfo {
-  type: string
-  title: string
-  description: string
-  iterationPath: string
-  areaPath: string
+  type: string;
+  title: string;
+  description: string;
+  iterationPath: string;
+  areaPath: string;
 }
 
 export async function createWorkItem(
@@ -16,7 +17,8 @@ export async function createWorkItem(
   project: string,
   workItemInfo: IWorkItemInfo
 ): Promise<number> {
-  const wiClient = await getWiClient(token, orgName)
+  const wiClient = await getWiClient(token, orgName);
+  core.debug('Got work item client');
 
   const patchDoc = [
     {op: 'add', path: '/fields/System.Title', value: workItemInfo.title},
@@ -25,41 +27,44 @@ export async function createWorkItem(
       path: '/fields/System.Description',
       value: workItemInfo.description
     }
-  ] as JsonPatchDocument
+  ] as JsonPatchDocument;
 
   if (workItemInfo.areaPath !== '') {
-    ;(patchDoc as any[]).push({
+    (patchDoc as any[]).push({
       op: 'add',
       path: '/fields/System.AreaPath',
       value: workItemInfo.areaPath
-    })
+    });
   }
   if (workItemInfo.iterationPath !== '') {
-    ;(patchDoc as any[]).push({
+    (patchDoc as any[]).push({
       op: 'add',
       path: '/fields/System.IterationPath',
       value: workItemInfo.iterationPath
-    })
+    });
   }
 
+  core.debug('Calling create work item...');
   const workItem = await wiClient.createWorkItem(
     null,
     patchDoc,
     workItemInfo.type,
     project
-  )
+  );
+
   if (workItem?.id === undefined) {
-    throw new Error('Work item was not created')
+    throw new Error('Work item was not created');
   }
-  return workItem.id
+
+  return workItem.id;
 }
 
 async function getWiClient(
   token: string,
   orgName: string
 ): Promise<IWorkItemTrackingApi> {
-  const orgUrl = `https://dev.azure.com/${orgName}`
-  const authHandler = azdev.getPersonalAccessTokenHandler(token)
-  const connection = new azdev.WebApi(orgUrl, authHandler)
-  return connection.getWorkItemTrackingApi()
+  const orgUrl = `https://dev.azure.com/${orgName}`;
+  const authHandler = azdev.getPersonalAccessTokenHandler(token);
+  const connection = new azdev.WebApi(orgUrl, authHandler);
+  return connection.getWorkItemTrackingApi();
 }
